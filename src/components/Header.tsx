c63e2a8fa41fea3a74be4440481b6d659a2149a2
Cristian Atencia <cristianatencia220673@correo.itm.edu.co>
2022-04-15 17:26:55 -0500

    header Cart dropdown

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,17 @@
+import { useContext } from "react";
 import { Badge, Container, Dropdown, FormControl, Nav, Navbar } from "react-bootstrap"
+import { AiFillDelete } from "react-icons/ai";
 
 
 import {FaShoppingCart} from 'react-icons/fa'
 import { Link } from "react-router-dom"
+import { CartContext } from "../context/cartContext";
 
 
 export const Header = () => {
+
+    const {cartState: { cart}, dispatch} = useContext(CartContext);
+
   return (
     <>
     <Navbar bg="dark" variant="dark" style={{height: 80}}>
@@ -25,11 +31,42 @@ export const Header = () => {
                 <Dropdown>
                     <Dropdown.Toggle variant="success">
                     <FaShoppingCart color="white" fontSize="25px"/>
-                        <Badge bg="success">10</Badge>
+                        <Badge bg="none">{cart.length}</Badge>
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu style={{minWidth: 370}}>
-                        <span style={{ padding: 10}}>Cart is Empty!</span>
+                        {
+                           cart.length > 0 ?
+                            (<>
+                            {
+                                cart.map((prod: any) => (
+                                    <span className="cartitem" key={prod.id}>
+                                    <img
+                                      src={prod.image}
+                                      className="cartItemImg"
+                                      alt={prod.name}
+                                    />
+                                    <div className="cartItemDetail">
+                                      <span>{prod.name}</span>
+                                      <span>₹ {prod.price.split(".")[0]}</span>
+                                    </div>
+                                    <AiFillDelete
+                                      fontSize="20px"
+                                      style={{ cursor: "pointer" }}
+                                      onClick={() =>
+                                        dispatch({
+                                          type: 'removeFromCart',
+                                          payload: prod,
+                                        })
+                                      }
+                                    />
+                                  </span>
+                                ))
+                            }
+                            </>):
+                            (<span style={{ padding: 10}}>Cart is Empty!</span>)
+                        }
+                       
 
                     </Dropdown.Menu>
 
